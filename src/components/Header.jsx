5b0985e256d4cd40e69e1e97019271761d41f17d
OmarMSDZ <Omarjadismoralesdiaz@gmail.com>
2024-08-17 00:50:28 -0400

    cambios en deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,41 @@
+import {React, useState} from 'react'
 
 export default function Header() {
-    return(
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => {
+    setIsNavCollapsed(!isNavCollapsed);
+  };
+
+  const closeNav = () => {
+    if (!isNavCollapsed) {
+      setIsNavCollapsed(true);
+    }
+  };
+
+  return(
         <>
        <div className="navbarcontainer">
         {/* <div className="container"> */}
         <nav class="navbar nav navbar-expand-lg navbar-custom" >
           <div class="container-fluid">
             <a class="navbar-brand" href="/home">&lt;OmarMSDZ/&gt;</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded={!isNavCollapsed ? true : false}  aria-label="Toggle navigation" onClick={handleNavCollapse}>
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
               <ul class="navbar-nav mx-auto">
                 <li class="nav-item">
-                  <a class="nav-link"  href="#About">About</a>
+                  <a class="nav-link"  href="#About" onClick={closeNav}>About</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#Skills">Skills</a>
+                  <a class="nav-link" href="#Skills" onClick={closeNav}>Skills</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#Projects">Projects</a>
+                  <a class="nav-link" href="#Projects" onClick={closeNav}>Projects</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#Contact">Contact</a>
+                  <a class="nav-link" href="#Contact" onClick={closeNav}>Contact</a>
                 </li>
               </ul>
             </div>
